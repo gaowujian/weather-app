0a9a57e0dd2843656933b0fc021026d24ac899ff
Tony <gwjlshebei@gmail.com>
2019-08-31 18:30:38 +1000

    Updates

---
+++ precache-manifest.a9e6f93c6462fb30d2d667634247b960.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e9587278f54ac65056cef89a05c9454d",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "0d8b50da5e781ca05fb1",
+    "url": "/weather-app/static/js/2.81448afb.chunk.js"
+  },
+  {
+    "revision": "09c04ec030bf91047b9a",
+    "url": "/weather-app/static/js/main.3ae97dc7.chunk.js"
+  },
+  {
+    "revision": "7a390137f1e79ffd409f",
+    "url": "/weather-app/static/js/runtime~main.caa3d5bf.js"
+  }
+]);
